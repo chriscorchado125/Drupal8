22e9924a26f1b95f480c46a9830b4ed09c1a3789
chriscorchado125 <chris.corchado125@gmail.com>
2020-10-06 12:13:01 -0400

    deleted ts and js files

--- themes/custom/chriscorchado/js/getMonthYear.js
+++
@@ -1,7 +0,0 @@
-function getMonthYear(dateString) {
-    let newDate = new Date(dateString);
-    return (newDate.toLocaleString('default', { month: 'long' }) +
-        ' ' +
-        newDate.getFullYear().toString());
-}
-;
