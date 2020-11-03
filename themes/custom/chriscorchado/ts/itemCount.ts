28df6ecb2dfa3d6b3057c27e91849e6d3a85bfb4
chriscorchado125 <chris.corchado125@gmail.com>
2020-11-03 10:20:54 -0500

    added comments to itemCount.js

--- themes/custom/chriscorchado/ts/itemCount.ts
+++ themes/custom/chriscorchado/ts/itemCount.ts
@@ -5,9 +5,6 @@ const MAX_ITEMS_PER_PAGE = 50;
  */
 const setItemCounts = () => {
 
-  let params = new URLSearchParams(document.location.search);
-  let count = parseInt(document.getElementById("record-total").innerText);
-
   let hasPreviousLink = false;
   let hasNextLink = false;
 
@@ -19,17 +16,20 @@ const setItemCounts = () => {
     hasNextLink = document.getElementsByClassName("pager-navigation")[1].nodeName == "A" ? true : false;
   }
 
-  let pageNum = 1;
-  let searchedFor = '';
+  let params = new URLSearchParams(document.location.search);
 
   // adjust page number - Drupals page numbers are zero indexed so page 0 is 1
+  let pageNum = 1;
   if(params.get("page")){
     pageNum = parseInt(params.get("page")) + 1;
   }
 
+  let searchedFor = '';
   if(params.get("search_api")){
     searchedFor = params.get("search_api");
   }
+
+  let count = parseInt(document.getElementById("record-total").innerText);
 
   // if there is a next or prev link then show the pagination
   if (hasPreviousLink || hasNextLink) {
@@ -37,6 +37,7 @@ const setItemCounts = () => {
     let lastRange = pageNum * MAX_ITEMS_PER_PAGE;
     let firstRange = lastRange - MAX_ITEMS_PER_PAGE;
 
+    // adjust firstRange to compensate for Drupals page numbers being zero indexed
     if(firstRange === 0){
       firstRange = 1;
     }else{
@@ -47,9 +48,12 @@ const setItemCounts = () => {
       lastRange = (firstRange + count) - 1;
     }
 
+    // add item counts to the page
+    document.getElementsByTagName("h1")[0].classList.add("paginationYes-H1");
     document.getElementById("search-container").className = "paginationYes";
     document.getElementById("searchCount").innerHTML = ` Items <span id="totalItems">${firstRange + "-" + lastRange}</span>`;
   } else {
+    document.getElementsByTagName("h1")[0].classList.add("paginationNo-H1");
     document.getElementById("search-container").className = "paginationNo";
     document.getElementById("searchCount").innerHTML = `<span id="totalItems">${count}</span> ${count == 1 ? "Item" : "Items"}`;
   }
@@ -63,6 +67,7 @@ const setPagination = () => {
   const DRUPAL_PAGER = document.querySelectorAll("ul.js-pager__items li a");
   let actualPrevLink = "", actualNextLink = "";
 
+  // find the previous and next links
   for (let i = 0; i < DRUPAL_PAGER.length; i++){
 
     if (DRUPAL_PAGER[i].getAttribute("rel") == "next") {
@@ -74,6 +79,7 @@ const setPagination = () => {
     }
   }
 
+  // configure the previous and next links
   const prevLink = actualPrevLink ?
     `<a href="${actualPrevLink}" class="pager-navigation" title="View the previous page" tabindex="10" role="button">Prev</a>`
     : `<span class="pager-navigation disabled" title="There is no previous page available" tabindex="11" role="button">Prev</span>`;
@@ -83,7 +89,10 @@ const setPagination = () => {
 
   if (actualPrevLink.length + actualNextLink.length) {
     document.getElementById("pagination").innerHTML = prevLink + nextLink;
+    // style.display set to block or inline block within the pagination media query
   } else {
     document.getElementById("pagination").innerHTML = "";
+    document.getElementById("pagination").style.display = "none";
   }
 }
+
