e80439976183fde408c409e47f17adeb073553c8
chriscorchado125 <chris.corchado125@gmail.com>
2020-11-05 11:46:10 -0500

    added check for search container

--- themes/custom/chriscorchado/js/search.js
+++ themes/custom/chriscorchado/js/search.js
@@ -4,28 +4,33 @@ const configureSearchForm = () => {
     if (location.pathname.toString().indexOf('/drupal8/') !== -1) {
         subFolder = "/drupal8";
     }
-    const SEARCH_CONTAINER = document.getElementById("search-container");
-    if (location.pathname.toString().indexOf("compan") !== -1) {
-        SEARCH_CONTAINER.setAttribute("action", subFolder + "/company-search");
-        clearSearchURL = subFolder + "/companies";
-    }
-    if (location.pathname.toString().indexOf("course") !== -1 || location.pathname.toString().indexOf("award") !== -1) {
-        SEARCH_CONTAINER.setAttribute("action", subFolder + "/award-search");
-        clearSearchURL = subFolder + "/courses";
-    }
-    if (location.pathname.toString().indexOf("project") !== -1) {
-        SEARCH_CONTAINER.setAttribute("action", subFolder + "/project-search");
-        clearSearchURL = subFolder + "/projects";
-    }
-    document.getElementById("searchClear").onclick = () => location.href = clearSearchURL;
-    let params = new URLSearchParams(document.location.search);
-    if (params.get("search_api")) {
-        document.getElementById("searchSite").value = params.get("search_api");
-        document.getElementById("searchSite").select();
-    }
-    else {
-        if (document.location.pathname == "/award-search") {
-            document.getElementById("searchSite").focus();
+    const checkForSearchContainer = setInterval(function () {
+        if (document.getElementById("search-container")) {
+            const SEARCH_CONTAINER = document.getElementById("search-container");
+            if (location.pathname.toString().indexOf("compan") !== -1) {
+                SEARCH_CONTAINER.setAttribute("action", subFolder + "/company-search");
+                clearSearchURL = subFolder + "/companies";
+            }
+            if (location.pathname.toString().indexOf("course") !== -1 || location.pathname.toString().indexOf("award") !== -1) {
+                SEARCH_CONTAINER.setAttribute("action", subFolder + "/award-search");
+                clearSearchURL = subFolder + "/courses";
+            }
+            if (location.pathname.toString().indexOf("project") !== -1) {
+                SEARCH_CONTAINER.setAttribute("action", subFolder + "/project-search");
+                clearSearchURL = subFolder + "/projects";
+            }
+            document.getElementById("searchClear").onclick = () => location.href = clearSearchURL;
+            let params = new URLSearchParams(document.location.search);
+            if (params.get("search_api")) {
+                document.getElementById("searchSite").value = params.get("search_api");
+                document.getElementById("searchSite").select();
+            }
+            else {
+                if (document.location.pathname == "/award-search") {
+                    document.getElementById("searchSite").focus();
+                }
+            }
+            clearInterval(checkForSearchContainer);
         }
-    }
+    }, 100);
 };
--- themes/custom/chriscorchado/ts/search.ts
+++ themes/custom/chriscorchado/ts/search.ts
@@ -6,40 +6,47 @@ const configureSearchForm = () => {
   let subFolder = "";
   let clearSearchURL = "";
 
+  // live site is hosted from the drupal8 folder
   if(location.pathname.toString().indexOf('/drupal8/') !== -1){
     subFolder = "/drupal8";
   }
 
-  const SEARCH_CONTAINER = document.getElementById("search-container");
+  const checkForSearchContainer = setInterval(function() {
+    if (document.getElementById("search-container")) {
 
-  if(location.pathname.toString().indexOf("compan") !== -1){
-    SEARCH_CONTAINER.setAttribute("action", subFolder + "/company-search");
-    clearSearchURL = subFolder + "/companies";
-  }
+      const SEARCH_CONTAINER = document.getElementById("search-container");
 
-  if(location.pathname.toString().indexOf("course") !== -1 || location.pathname.toString().indexOf("award") !== -1){
-    SEARCH_CONTAINER.setAttribute("action", subFolder + "/award-search");
-    clearSearchURL = subFolder + "/courses";
-  }
+      if(location.pathname.toString().indexOf("compan") !== -1){
+        SEARCH_CONTAINER.setAttribute("action", subFolder + "/company-search");
+        clearSearchURL = subFolder + "/companies";
+      }
 
-  if(location.pathname.toString().indexOf("project") !== -1){
-    SEARCH_CONTAINER.setAttribute("action", subFolder + "/project-search");
-     clearSearchURL = subFolder + "/projects";
-  }
+      if(location.pathname.toString().indexOf("course") !== -1 || location.pathname.toString().indexOf("award") !== -1){
+        SEARCH_CONTAINER.setAttribute("action", subFolder + "/award-search");
+        clearSearchURL = subFolder + "/courses";
+      }
 
-  document.getElementById("searchClear").onclick = () => location.href = clearSearchURL;
+      if(location.pathname.toString().indexOf("project") !== -1){
+        SEARCH_CONTAINER.setAttribute("action", subFolder + "/project-search");
+        clearSearchURL = subFolder + "/projects";
+      }
 
+      document.getElementById("searchClear").onclick = () => location.href = clearSearchURL;
 
-  let params = new URLSearchParams(document.location.search);
+      let params = new URLSearchParams(document.location.search);
 
-  // if searching then set searched value and select it
-  if(params.get("search_api")){
-    (<HTMLInputElement>document.getElementById("searchSite")).value = params.get("search_api");
-    (<HTMLInputElement>document.getElementById("searchSite")).select();
-  } else {
-    // search was cleared so set focus back to search input
-    if(document.location.pathname == "/award-search"){
-      (<HTMLInputElement>document.getElementById("searchSite")).focus();
+      // if searching then set searched value and select it
+      if(params.get("search_api")){
+        (<HTMLInputElement>document.getElementById("searchSite")).value = params.get("search_api");
+        (<HTMLInputElement>document.getElementById("searchSite")).select();
+      } else {
+        // search was cleared so set focus back to search input
+        if(document.location.pathname == "/award-search"){
+          (<HTMLInputElement>document.getElementById("searchSite")).focus();
+        }
+      }
+
+      clearInterval(checkForSearchContainer);
     }
-  }
+  }, 100);
 }
