c7bbe425d17a73a770728c4a3baf3a2c6560ab11
chriscorchado125 <chris.corchado125@gmail.com>
2020-11-06 20:52:01 -0500

    removed redundant pageLoaded file

--- themes/custom/chriscorchado/js/pageLoaded.js
+++
@@ -1,9 +0,0 @@
-import { setPagination, setItemCounts } from "./itemCount.js";
-import { configureSearchForm } from "./search.js";
-import { setNavItem } from "./setNavItem.js";
-window.addEventListener('DOMContentLoaded', () => {
-    setPagination();
-    setItemCounts();
-    setNavItem();
-    configureSearchForm();
-});
--- themes/custom/chriscorchado/ts/drupalBehaviors.ts
+++ themes/custom/chriscorchado/ts/drupalBehaviors.ts
@@ -4,7 +4,6 @@ import { setNavItem } from "./setNavItem.js";
 
 /**
  * Setup pagination, page counts, main navigation items and search
- * This exist on pageLoaded.ts and drupalBehaviors.ts
  */
 (function ($, Drupal) {
   Drupal.behaviors.chriscorchado = {
