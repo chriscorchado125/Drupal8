fcaf2c83c881742b3e2d2ff8f8136e081f6b6ef8
chriscorchado125 <chris.corchado125@gmail.com>
2020-12-14 16:46:23 -0500

    updated animateLogo to also work on click

--- themes/custom/chriscorchado/js/drupalBehaviors.js
+++ themes/custom/chriscorchado/js/drupalBehaviors.js
@@ -13,7 +13,22 @@ import { animateLogo } from './animateLogo.js';
             configureSearchForm();
         }
     };
-})(window.jQuery, window.Drupal);
+})(Query, Drupal);
+animateLogo('logo-image', 'spin');
+document.addEventListener('click', function (event) {
+    const itemClasses = event.target.className.toString();
+    const validClasses = ['logo-image', 'nav-item', 'search-btn', 'pager-navigation', 'resume-link'];
+    let okToAnimate = validClasses.some(el => itemClasses.includes(el));
+    const searchButton = ['search-btn click'];
+    const searchClicked = searchButton.some(el => itemClasses.includes(el));
+    const searchValue = document.getElementById('search-site');
+    if (searchClicked && searchValue.value === '') {
+        okToAnimate = false;
+    }
+    if (okToAnimate) {
+        animateLogo('logo-image', 'spin');
+    }
+});
 window.onload = function () {
     animateLogo('logo-image', '');
 };
--- themes/custom/chriscorchado/ts/drupalBehaviors.ts
+++ themes/custom/chriscorchado/ts/drupalBehaviors.ts
@@ -17,7 +17,29 @@ import { animateLogo } from './animateLogo.js'
       configureSearchForm()
     }
   }
-})(window.jQuery, window.Drupal)
+})(Query, Drupal)
+
+animateLogo('logo-image', 'spin')
+
+// Animate logo when the navigation, search, pagination or resume links are clicked because the pages wait to load
+document.addEventListener('click', function (event) {
+  const itemClasses = event.target.className.toString()
+  const validClasses = ['logo-image', 'nav-item', 'search-btn', 'pager-navigation', 'resume-link']
+  let okToAnimate = validClasses.some(el => itemClasses.includes(el))
+
+  const searchButton = ['search-btn click']
+  const searchClicked = searchButton.some(el => itemClasses.includes(el))
+  const searchValue = document.getElementById('search-site')
+
+  // Don't animate if searching for an empty value
+  if(searchClicked && searchValue.value === ''){
+    okToAnimate = false
+  }
+
+  if (okToAnimate){
+    animateLogo('logo-image', 'spin')
+  }
+})
 
 window.onload = function() {
   animateLogo('logo-image', '')
